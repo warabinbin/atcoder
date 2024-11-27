b321d7572809a0d72de3e000546c9506ca257ef8
mogit <mochi.warabii@gmail.com>
2024-11-28 05:07:11 +0900

    add 381_C

--- 381/C_381.java
+++ 381/C_381.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
 public class C_381 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        int len = input.nextInt();
+        input.nextLine();
+        String word = input.nextLine();
+
+        int max = 0;
+        for(int i = 0; i < len; i++) {
+            if(word.charAt(i) == '/') {
+                max = Math.max(max, find(word, i));
+            }
+        }
+
+        System.out.println(max);
+    }
+    public static int find(String word, int start) {
+        int left = start - 1, right = start + 1;
+        while(left >= 0 && right < word.length()) {
+            if(word.charAt(left) == '1' && word.charAt(right) == '2') {
+                left--;
+                right++;
+            }
+            else break;
+        }
+        return right - left - 1;
+    }
 }
